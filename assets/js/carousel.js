384a9995730a228c6bfb1c5ee034108fb0150910
joao.souza <joao.souza@cofen.gov.br>
2025-12-17 10:21:32 -0300

    responsividade no carrousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,9 +1,10 @@
 export function initCarousel() {
   const track = document.getElementById('carousel-track');
+  const slides = track.children;
   let index = 0;
 
   setInterval(() => {
-    index = (index + 1) % track.children.length;
-    track.style.transform = `translateX(${-index * 100}%)`;
+    index = (index + 1) % slides.length;
+    track.style.transform = `translateX(-${index * 100}%)`;
   }, 3000);
 }
